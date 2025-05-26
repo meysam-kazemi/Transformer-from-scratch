cdb7514b42592d287016ae47846d938e40a94b14
meysam-kazemi <meisam.daysu@gmail.com>
2025-05-26 14:42:15 +0330

    Fix build vocab in data code

--- data/dummy_data.py
+++ data/dummy_data.py
@@ -4,13 +4,13 @@ from datasets import load_dataset
 import nltk
 nltk.download('punkt')
 nltk.download('punkt_tab')
-from nltk.tokenize import word_tokenizer
-from counter import Counter
+from nltk.tokenize import word_tokenize
+from collections import Counter
 
-dataset = load_dataset("opus-books", "en-nl", split="train")
+dataset = load_dataset("opus_books", "en-nl", split="train")
 
-SRC_LNG = "nl"
-TGT_LNG = "en"
+SRC_LNG = "en"
+TGT_LNG = "nl"
 
 special_symbols = ["<unk>", "<pad>", "bos", "eos"]
 UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
@@ -19,12 +19,17 @@ UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
 def build_vocab(dataset, lang):
     counter = Counter()
     for item in dataset:
-        counter.update(word_tokenizer(item['translation'][lang]))
+        counter.update(word_tokenize(item['translation'][lang]))
     vocab = {sym:i for i, sym in enumerate(special_symbols)}
     for word in counter:
         if word not in vocab:
             vocab[word] = len(vocab)
     return vocab
 
+import pdb; pdb.set_trace()
+src_vocab = build_vocab(dataset, SRC_LNG)
+tgt_vocab = build_vocab(dataset, TGT_LNG)
+
+print(type(src_vocab))
 
 
